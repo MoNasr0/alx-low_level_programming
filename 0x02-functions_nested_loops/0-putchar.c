24c095394f290b97c185b511b949c9d193535ed9
Mohamed Nasr <mnasr10924@gmail.com>
2023-03-21 15:40:55 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,21 @@
 #include "main.h"
-/*
-* Write a program that prints _putchar, followed by a new line.
-*/
-int main(void){
-		_putchar("_putcher");
-		_putchar('\n');
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
+
