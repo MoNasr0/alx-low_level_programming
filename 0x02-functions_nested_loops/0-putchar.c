b003d722d7fbe5510134c6a1c406eb9511c4b2dc
Mohamed Nasr <mnasr10924@gmail.com>
2023-03-21 15:28:52 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,14 @@
 #include "main.h"
+
 /**
  * main - Entry point
  * Return: Always 0
  */
-int main(void){
+
+int main(void)
+
+{
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -14,4 +19,5 @@ int main(void){
 	_putchar('r');
 	_putchar('\n');
 	return (0);
+
 }
